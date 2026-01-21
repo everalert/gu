a0902cb991014ed1fa0baf4ddc45bea9ffee4f8b
Nathaniel <everalert@users.noreply.github.com>
2026-01-22 03:49:51 +1100

    chore: clean up build script

--- build.zig
+++ build.zig
@@ -1,51 +1,116 @@
 const std = @import("std");
+const builtin = @import("builtin");
+
+const Build = std.Build;
+const ResolvedTarget = Build.ResolvedTarget;
+const OptimizeMode = std.builtin.OptimizeMode;
+const Step = Build.Step;
+const StepMakeOptions = Step.MakeOptions;
+const StepCompile = Step.Compile;
+
+// TODO: module export
+// TODO: tests
+// TODO: linux builds (see: castholm/SDL_linux_deps)
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+    const opts: GlobalOptions = .Init(b);
 
-    const exe = b.addExecutable(.{
-        .name = "galeforce-ui",
+    const example = bin_example(b, &opts);
+    step_example(b, example, &opts);
+    step_example_run(b, example);
+
+    step_clean(b);
+}
+
+// EXAMPLE APP
+
+fn bin_example(b: *Build, opts: *const GlobalOptions) *StepCompile {
+    const bin = b.addExecutable(.{
+        .name = "gu",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
+            //.imports = imports, // []const Build.Module.Import
+            .target = opts.Target,
+            .optimize = opts.Optimize,
+            .strip = opts.Strip,
         }),
     });
 
-    // TODO: place behind 'example' step
-    const sdl_dep = b.lazyDependency("sdl", .{
-        .target = target,
-        .optimize = optimize,
-    });
-    if (sdl_dep) |dep| {
+    const module_opts = .{ .target = opts.Target, .optimize = opts.Optimize, .strip = opts.Strip };
+    if (b.lazyDependency("sdl", module_opts)) |dep| {
         const sdl_lib = dep.artifact("SDL3");
-        exe.root_module.linkLibrary(sdl_lib);
+        bin.root_module.linkLibrary(sdl_lib);
     }
 
-    exe.root_module.addAnonymousImport("ascii-font", .{
-        .root_source_file = b.path("assets/ascii.bmp"),
-    });
-    exe.root_module.addAnonymousImport("corner-round", .{
-        .root_source_file = b.path("assets/corner-round.bmp"),
-    });
-    exe.root_module.addAnonymousImport("corner-angular", .{
-        .root_source_file = b.path("assets/corner-angular.bmp"),
-    });
-    exe.root_module.addAnonymousImport("corner-beveled", .{
-        .root_source_file = b.path("assets/corner-beveled.bmp"),
-    });
+    const assets = [_]struct { []const u8, []const u8 }{
+        .{ "ascii-font", "assets/ascii.bmp" },
+        .{ "corner-round", "assets/corner-round.bmp" },
+        .{ "corner-angular", "assets/corner-angular.bmp" },
+        .{ "corner-beveled", "assets/corner-beveled.bmp" },
+    };
+    for (assets) |asset|
+        bin.root_module.addAnonymousImport(asset[0], .{ .root_source_file = b.path(asset[1]) });
 
-    b.installArtifact(exe);
-
-    // run step
-    // TODO: maybe also place this behind 'example' step?
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args|
-        run_cmd.addArgs(args);
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    return bin;
 }
+
+fn step_example(
+    b: *Build,
+    bin: *StepCompile,
+    opts: *const GlobalOptions,
+) void {
+    const step = b.step("example", "Build the example app");
+
+    if (opts.NoBin) {
+        step.dependOn(&bin.step);
+    } else {
+        b.installArtifact(bin);
+    }
+}
+
+fn step_example_run(b: *Build, bin: *StepCompile) void {
+    const step = b.step("example-run", "Build and run the example app");
+
+    const command = b.addRunArtifact(bin);
+    if (b.args) |args| command.addArgs(args);
+
+    step.dependOn(&command.step);
+}
+
+// CLEANUP / BUILD UTIL
+
+fn step_clean(b: *Build) void {
+    const step = b.step("clean", "Remove build system artifacts");
+
+    step.dependOn(&b.addRemoveDirTree(.{ .cwd_relative = b.install_path }).step);
+
+    if (builtin.os.tag != .windows) {
+        step.dependOn(&b.addRemoveDirTree(.{ .cwd_relative = ".zig-cache" }).step);
+    } else {
+        step.makeFn = CleanWindows;
+    }
+}
+
+fn CleanWindows(_: *Step, _: StepMakeOptions) anyerror!void {
+    // Windows locks `.zig-cache` during build, so we have to clean it outside the build system
+    std.log.err("Cannot remove `.zig-cache` during build process on Windows. Run `./clean.bat`", .{});
+}
+
+// GLOBAL OPTIONS
+
+/// Common options for this build script
+const GlobalOptions = struct {
+    Target: ResolvedTarget,
+    Optimize: OptimizeMode,
+    NoBin: bool,
+    Strip: bool,
+
+    pub fn Init(b: *Build) GlobalOptions {
+        return .{
+            .Target = b.standardTargetOptions(.{}),
+            .Optimize = b.standardOptimizeOption(.{}),
+            .NoBin = b.option(bool, "no-bin", "skip emitting binary") orelse false,
+            .Strip = b.option(bool, "strip", "strip the binary") orelse false,
+        };
+    }
+};
