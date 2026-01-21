77e74511bb73d095b958ed97ffe0eef5d47c75e7
Nathaniel <everalert@users.noreply.github.com>
2026-01-22 02:22:41 +1100

    fix: build script compiling on 0.15.2

--- build.zig
+++ build.zig
@@ -6,9 +6,11 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "galeforce-ui",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
 
     // TODO: place behind 'example' step
